ac4860f6670ffa6bc664200294cbfdeca09b5eff
Heitor JC <heitorjcastilho@gmail.com>
2022-07-01 16:52:30 -0300

    ToDo List Files
    Create with firebase

---
+++ js/init.js
@@ -0,0 +1,13 @@
+// Such info must be obtained from Firebase Project, on firebase console
+var firebaseConfig = {
+  apiKey: "AIzaSyCMyFJLYChUof2lv1DEJbzC829BcB9nRhk",
+  authDomain: "wenner-ml.firebaseapp.com",
+  databaseURL: "https://wenner-ml-default-rtdb.firebaseio.com",
+  projectId: "wenner-ml",
+  storageBucket: "wenner-ml.appspot.com",
+  messagingSenderId: "355028069781",
+  appId: "1:355028069781:web:d90049d7bac522145d1a9c",
+  measurementId: "G-ZRYGXF3RTP"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
